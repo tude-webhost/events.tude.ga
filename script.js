8bb183b00d6d58edced92d9ed6adf1ea457ff4f4
maanex <maan91315@gmail.com>
2019-08-21 12:38:12 +0200

    init

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+
+Vue.component('panel', {
+    props: [ 'name' ],
+    template: '#t-panel'
+});
+
+Vue.component('box', {
+    props: [ ],
+    template: '#t-box'
+});
+
+var app;
+app = new Vue({
+    el: '#app',
+    data: {
+        scrolled: false,
+        panel: 'news'
+    }
+});
+
+var contentel = document.getElementById('content');
+contentel.onscroll = () => app.scrolled = contentel.scrollTop != 0;
+
+document.addEventListener('mousemove', function(e) {
+    var bounds;
+    for (var obj of document.getElementsByClassName('box')) {
+        if (obj.parentElement.querySelector(':hover') !== obj) continue;
+        bounds = obj.getBoundingClientRect();
+        let xAxis = (bounds.left+(bounds.right-bounds.left)/2 - e.pageX) / 100;
+        let yAxis = -(bounds.top+(bounds.bottom-bounds.top)/2 - e.pageY) / 50;
+        obj.style.setProperty('--transf', `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`);
+    }
+});
